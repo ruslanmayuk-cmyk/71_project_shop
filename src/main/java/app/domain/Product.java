04e125e46fe4abe99e25b54f9890279378b16932
ruslanmayuk-cmyk <ruslanmayuk@gmail.com>
2025-08-29 21:36:13 +0300

    Add task 1 - domain

--- src/main/java/app/domain/Product.java
+++ src/main/java/app/domain/Product.java
@@ -1,4 +1,70 @@
 package app.domain;
 
+import java.util.Objects;
+
 public class Product {
+    private  int id;
+    private String title;
+    private double price;
+    private boolean active;
+
+    public Product() {
+    }
+
+    public Product(boolean active, double price, String title, int id) {
+        this.active = active;
+        this.price = price;
+        this.title = title;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id == product.id && Double.compare(price, product.price) == 0 && active == product.active && Objects.equals(title, product.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, price, active);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Продукт: id - %d, наименование - %s, цена - %.2f, активен - %b.",
+                id, title, price, active);
+    }
 }
